3cddd98fe103f6745cda225da02ddb1d7d263ba2
sshersh <45491470+sshersh@users.noreply.github.com>
2019-04-25 00:26:30 -0400

    Add files via upload

---
+++ ofApp.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "game.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+		void setup();
+		void update();
+		void draw();
+
+		void mouseMoved(int x, int y );
+		void mouseDragged(int x, int y, int button);
+		void mousePressed(int x, int y, int button);
+		void mouseReleased(int x, int y, int button);
+		void mouseEntered(int x, int y);
+		void mouseExited(int x, int y);
+		void windowResized(int w, int h);
+		void dragEvent(ofDragInfo dragInfo);
+		void gotMessage(ofMessage msg);
+		
+};
+
+
