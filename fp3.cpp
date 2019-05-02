3b358016a844d4833c281e4b6dd53ed117eb597c
sshersh <45491470+sshersh@users.noreply.github.com>
2019-05-02 16:18:55 -0400

    Bugs fixed

--- fp3.cpp
+++ fp3.cpp
@@ -16,20 +16,23 @@ using namespace std;
 
 class tBoard : public Board {
     public:
+        tBoard() : Board() {}
         void revealtile(int r, int c);
         void flagtile(int r, int c);
         void unflagtile(int r, int c);
         void drawboard();
         void drawtile(int r, int c);
-        void placebombs(int r, int c);
+        void placebombs(int r, int c); //the arguments are the user's first coordinates
         void check_empty(int r, int c);
         int check_win();
         bool freesquare(int r1, int r2, int r, int c);
+        void revealBoard();
+        void reset();
 };
 
 class tGame : public Game {
     public:
-        tGame() {
+        tGame() : Game() {
             b = new tBoard;
         }
         ~tGame() {
@@ -42,7 +45,35 @@ class tGame : public Game {
         void asktile(int& r, int& c, char& i);
         void firstbox(int& row, int& col, char& item);
         void play_game();
+        bool playermove(int row, int col, char item);
+        //void print_Records();
+        //void check_Record();
 };
+
+// class tRecord : public Record {
+//     public:
+//         tRecord(int r, int c, time_t& begin, time_t& end, diffs di)
+//             rows = r;
+//             cols = c;
+//             d = di;
+//             time = difftime(begin, end);
+//         }
+//         bool operator>(Record& r) {
+//             int size = rows * cols;
+//             int size2 = r.rows * r.cols;
+//             if(size != size2) {
+//                 return size > size2;
+//             }
+//             else if(d != r.d) {
+//                 return d > r.d;
+//             }
+//             else {
+//                 return time <= r.time;
+//             }
+//         }
+//         void printRecord();
+// };
+//
 
 
 bool tBoard::freesquare(int r1, int r2, int r, int c) {
@@ -140,7 +171,6 @@ void tBoard::drawtile(int r, int c) {
         return;
 }
 
-        // ---------------------------------------------//
 
 void tBoard::drawboard() {
         int row;
@@ -281,6 +311,7 @@ void tGame::check_boardsize() {
 }
 
 void tGame::check_difficulty() {
+        char difficulty;
         do {
                 cout << "Easy (E or e): ~12% Bombs" << endl;
                 cout << "Medium (M or m): ~16% Bombs" << endl;
@@ -291,12 +322,15 @@ void tGame::check_difficulty() {
 
         if(difficulty == 'e' || difficulty == 'E') {
                 b->bombs = (b->rowl * b->coll) * .12;
+                d = easy;
         }
         else if(difficulty == 'm' || difficulty == 'M') {
                 b->bombs = (b->rowl * b->coll) * .16;
+                d = medium;
         }
         else {
                 b->bombs = (b->rowl * b->coll) * .18;
+                d = hard;
         }
 }
 
@@ -369,43 +403,91 @@ void tGame::firstbox(int& row, int& col, char& item) {
                 } while (item != 'U' && item != 'u');
 }
 
+bool tGame::playermove(int row, int col, char item) {
+        if(item == 'p' || item == 'P') {
+                b->flagtile(row-1, col-1);
+        }
+        if(item == 'r' || item == 'R') {
+                b->unflagtile(row-1, col-1);
+        }
+        if(item == 'u' || item == 'U') {
+                if(b->t[row-1][col-1].bomb == 1) {
+                        b->revealBoard();
+                        return 1;
+                }
+                else {
+                        b->revealtile(row-1, col-1);
+                        b->check_empty(row-1, col-1);
+                }
+        }
+        return 0;
+}
+
+void tBoard::revealBoard() {
+    for (int r = 0; r < rowl; r++) {
+            for (int c = 0; c < coll; c++) {
+                    t[r][c].flag = 0;
+                    t[r][c].show = 1;
+            }
+    }
+    drawboard();
+}
+
 void tGame::play_game() {
         int row, col, n;
         char item;
         firstbox(row, col, item);
+        //begin = time(0);
         b->placebombs(row, col);
         b->check_empty(row-1, col -1);
         while(1) {
                 b->drawboard();
                 asktile(row, col, item);
-                if(item == 'p' || item == 'P') {
-                        b->flagtile(row-1, col-1);
-                }
-                if(item == 'r' || item == 'R') {
-                        b->unflagtile(row-1, col-1);
-                }
-                if(item == 'u' || item == 'U') {
-                        if(b->t[row-1][col-1].bomb == 1) {
-                                for (int r = 0; r < b->rowl; r++) {
-                                        for (int c = 0; c < b->coll; c++) {
-                                                b->t[r][c].flag = 0;
-                                                b->t[r][c].show = 1;
-                                        }
-                                }
-                                b->drawboard();
-                                cout << "You Lost!" << endl;
-                                return;
-                        }
-                        else {
-                                b->revealtile(row-1, col-1);
-                                b->check_empty(row-1, col-1);
-                        }
+                if(playermove(row, col, item)) {
+                    cout << "You Lost!" << endl;
+                    return;
                 }
                 if(b->check_win()) {
                         cout << "You Won!" << endl;
+                        //end = time(0);
+                        //check_Record();
                         return;
                 }
         }
+}
+
+// void tRecord::printRecord() {
+//     cout << "Time: " << time << "\t";
+//     cout << "Difficulty: " << d << "\t";
+//     cout << "Board size" << rows << "x" << cols << endl;
+// }
+//
+// void tGame::print_Records() {
+//     cout << "NEW RECORD!" << endl;
+//     for(int ii = 0; ii < records.size(); ii++) {
+//         cout << ii + 1 << "." << "\t";
+//         records[ii]->printRecord();
+//     }
+// }
+//
+// void tGame::check_Record() {
+//     tRecord* thisGame = new tRecord(b->rowl, b->coll, begin, end, d);
+//     int n = records.size();
+//     if(*thisGame > *(records[n - 1])) {
+//         records.push_back(thisGame);
+//         print_Records();
+//         }
+//     else {
+//         delete thisGame;
+//     }
+// }
+
+void tBoard::reset() {
+        t.erase(t.begin(), t.end());
+        rowl = 0;
+        coll = 0;
+        bombs = 0;
+        tilesShown = 0;
 }
 
 int main() {
@@ -417,6 +499,8 @@ int main() {
                 if (!game.play_again()) {
                         break;
                 }
+                game.b->reset();
         }
         return 0;
 }
+
